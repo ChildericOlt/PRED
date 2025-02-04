fb868d4c56bcfa7c10d3b5d57a4d23917a22ac50
allan.guillard <allan.guillard@gmail.com>
2025-02-04 16:50:59 +0100

    début ml + modif fonction eval

--- eval.py
+++ eval.py
@@ -24,29 +24,38 @@ def notation(X_test, y_test, y_pred, model) :
     print(f'Confusion Matrix:\n{conf_matrix}')
 
     n_classes = len(np.unique(y_test))
-    y_test_bin = label_binarize(y_test, classes=np.unique(y_test))
     y_pred_proba = model.predict_proba(X_test)
 
-    palette = sns.color_palette("husl", n_classes)
+    if n_classes > 2:
+        y_test_bin = label_binarize(y_test, classes=np.unique(y_test))
+    else:
+        y_test_bin = y_test  # Pas besoin de binariser pour une classification binaire
+
+    palette = sns.color_palette("husl", max(n_classes, 3))  
 
     plt.figure(figsize=(8, 6))
 
-    for i in range(n_classes):
-        fpr, tpr, _ = roc_curve(y_test_bin[:, i], y_pred_proba[:, i])
+    if n_classes > 2:
+        for i in range(n_classes):
+            fpr, tpr, _ = roc_curve(y_test_bin[:, i], y_pred_proba[:, i])
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, color=palette[i], label=f'Classe {i} (AUC = {roc_auc:.2f})')
+    else:
+        fpr, tpr, _ = roc_curve(y_test_bin, y_pred_proba[:, 1])
         roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, color=palette[i], label=f'Classe {i} (AUC = {roc_auc:.2f})')
+        plt.plot(fpr, tpr, color='blue', label=f'ROC Curve (AUC = {roc_auc:.2f})')
 
     plt.plot([0, 1], [0, 1], 'k--', label="Random (AUC = 0.50)")
 
     plt.xlabel('Taux de faux positifs (FPR)')
     plt.ylabel('Taux de vrais positifs (TPR)')
-    plt.title('Courbes ROC pour classification multiclasses')
+    plt.title('Courbes ROC')
     plt.legend()
     plt.grid()
     plt.show()
 
-    auc_roc = roc_auc_score(y_test, model.predict_proba(X_test), multi_class='ovo')
-    print(f'AUC-ROC: {auc_roc}')
+    auc_roc = roc_auc_score(y_test,y_test_bin, multi_class='ovo')
+    return accuracy, precision, recall, f1, conf_matrix, auc_roc
 
 
 # utilisation : 
